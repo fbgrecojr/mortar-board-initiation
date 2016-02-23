0550a9ea4ab0aa8c31024a66000eba0f5e9a8f9f
Frank B Greco Jr <fbgrecojr@me.com>
2016-02-23 15:59:50 -0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const   express = require("express"),		
+        app     = express(),						
+        server  = require('http').createServer(app),
+        io		= require('socket.io')(server),
+        port	= process.env.PORT || 8080;
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use('/', express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+    res.render('index', {});
+});
+
+app.listen(port, function(){
+    console.log("LISTENING " + port);
+});
